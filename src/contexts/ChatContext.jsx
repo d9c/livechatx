40a0e23521094a76bcd3845fb594b1d1b344e3d3
d9c <david142x@gmail.com>
2022-08-08 19:49:56 -0300

    Update ChatContext.jsx

--- src/contexts/ChatContext.jsx
+++ src/contexts/ChatContext.jsx
@@ -6,20 +6,20 @@ export const ChatContext = createContext({});
 const socket = io.connect("https://d9c-livechat-backend.herokuapp.com");
 
 export const ChatContextProvider = ({ children }) => {
-  const [showChat, setShowChat] = useState(false);
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   return (
     <ChatContext.Provider
       value={{
         socket,
-        showChat,
-        setShowChat,
         username,
         setUsername,
         room,
         setRoom,
+        showChat,
+        setShowChat,
       }}
     >
       {children}
