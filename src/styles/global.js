ca34499d6064a4eb5ae3b2e8682b2604e1b1092e
d9c <david142x@gmail.com>
2022-08-26 09:25:56 -0300

    Update global.js

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,12 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle({
-  "*": {
-    margin: "0",
-    padding: "0",
-    boxSizing: "border-box",
-  },
-  body: {
-    backgroundColor: (props) => props.theme.background,
-  },
-});
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${(props) => props.theme.background};
+  }
+`;
