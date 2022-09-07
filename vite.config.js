7559a0371abc4bc18f4f37fd3bd38a081a5f94f5
d9c <david142x@gmail.com>
2022-09-07 20:48:09 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://d9c.github.io/livechat-frontend/",
+  base: "https://livechat-frontend.pages.dev/",
   server: {
     port: 3000,
   },
