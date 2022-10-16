68791d412474d6a2a07577b4f971cd2dc130c7bf
d9c <david142x@gmail.com>
2022-10-16 18:54:02 -0300

    Display URLs in messages

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -3,11 +3,28 @@ import React from "react";
 import * as S from "./styles";
 
 export const Message = ({ name, text, timestamp, $isSent }) => {
+  const linkify = (text) => {
+    const urlRegex =
+      /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+
+    const words = text.split(" ");
+
+    return words.map((word, index) => {
+      return word.match(urlRegex) ? (
+        <a key={index} href={word} target="_blank" style={{ color: "#80ff80" }}>
+          {word}
+        </a>
+      ) : (
+        ` ${word} `
+      );
+    });
+  };
+
   return (
     <S.Container>
       <S.Content $isSent={$isSent}>
         <S.Info $isSent={$isSent}>{name}</S.Info>
-        <S.Message>{text}</S.Message>
+        <S.Message>{linkify(text)}</S.Message>
         <S.Info $isSent={$isSent}>{timestamp}</S.Info>
       </S.Content>
     </S.Container>
