bbc05b219cf582c085b6a2c497d8e1ebb9d62db3
d9c <david142x@gmail.com>
2022-10-16 18:53:50 -0300

    Move import

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import * as S from "./App.styles";
-
 import { Chat } from "./components/Chat";
 import { UserList } from "./components/UserList";
 import { JoinRoom } from "./components/JoinRoom";
@@ -13,6 +11,8 @@ import { SnackbarContextProvider } from "./contexts/SnackbarContext";
 
 import { DarkTheme } from "./styles/themes/dark";
 import { GlobalStyle } from "./styles/global";
+
+import * as S from "./App.styles";
 
 export const App = () => {
   return (
