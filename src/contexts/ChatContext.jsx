9b9b22dd9b0fb5dd7534e048f1d205c99487db0b
d9c <david142x@gmail.com>
2022-10-17 23:08:23 -0300

    Update ChatContext.jsx

--- src/contexts/ChatContext.jsx
+++ src/contexts/ChatContext.jsx
@@ -3,11 +3,11 @@ import io from "socket.io-client";
 
 export const ChatContext = createContext({});
 
-let url = "https://livechat-backend.up.railway.app";
-if (process.env.NODE_ENV === "development") {
-  url = "http://localhost:4000";
-}
-const socket = io.connect(url);
+const backendUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4000"
+    : "https://livechat-backend.up.railway.app";
+const socket = io.connect(backendUrl);
 
 export const ChatContextProvider = ({ children }) => {
   const [name, setName] = useState("");
