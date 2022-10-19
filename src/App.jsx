7b8e222ce015133903ed8fc06cabacca14b00c0d
d9c <david142x@gmail.com>
2022-10-19 19:08:19 -0300

    Remove ThemeSwitcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import React, { useContext } from "react";
+import React from "react";
 import { ThemeProvider } from "styled-components";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import { ThemeContext } from "./contexts/ThemeContext";
 import { ChatContextProvider } from "./contexts/ChatContext";
 import { SnackbarContextProvider } from "./contexts/SnackbarContext";
 
-import { ThemeSwitcher } from "./components/ThemeSwitcher";
 import { JoinRoom } from "./components/JoinRoom";
 import { Chat } from "./components/Chat";
 import { UserList } from "./components/UserList";
 
+import { dark } from "./styles/themes/dark";
 import { GlobalStyle } from "./styles/global";
 
 import * as S from "./App.styles";
 
 export const App = () => {
-  const { theme } = useContext(ThemeContext);
-
   return (
-    <ThemeProvider theme={theme}>
-      <ThemeSwitcher />
+    <ThemeProvider theme={dark}>
       <ChatContextProvider>
         <SnackbarContextProvider>
           <S.Container>
