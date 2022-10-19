787ea863b6348dd125283267f860a750f8c0c631
d9c <david142x@gmail.com>
2022-10-19 19:16:04 -0300

    Remove ThemeContext

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import { ThemeContextProvider } from "./contexts/ThemeContext";
-
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemeContextProvider>
-    <Router>
-      <App />
-    </Router>
-  </ThemeContextProvider>
+  <Router>
+    <App />
+  </Router>
 );
