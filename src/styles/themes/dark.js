118a6e0d1997b53b600b6a662dd8009139457b46
d9c <david142x@gmail.com>
2022-10-22 14:35:03 -0300

    Update dark theme colors

--- src/styles/themes/dark.js
+++ src/styles/themes/dark.js
@@ -1,14 +1,20 @@
 export const dark = {
   title: "dark",
-  background: "#0a0a0a",
+  background: "#0b0b0b",
   container: {
-    header: "#111111",
-    body: "#0a0a0a",
+    header: "#0b0b0b",
+    body: "#0b0b0b",
   },
   text: "#ffffff",
   border: "#333333",
   message: {
-    sent: "#222222",
-    received: "#111111",
+    sent: "#3d3d3d",
+    received: "#1f1f1f",
   },
 };
+
+// #0b0b0b = rgb(11, 11, 11)
+// #ffffff = rgb(255, 255, 255)
+// #333333 = rgb(51, 51, 51)
+// #3d3d3d = rgb(61, 61, 61)
+// #1f1f1f = rgb(31, 31, 31)
